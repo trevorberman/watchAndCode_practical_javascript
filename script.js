9512f4825c4707501b96a9e1cef8217411190ae6
trevorberman <trevorberman@gmail.com>
2018-12-12 19:26:56 -0600

    Add version 2 coursework.

--- script.js
+++ script.js
@@ -3,17 +3,50 @@
 // V1 Requirements
 // -------------------------------------
 // [x] It should have a place to store todos
-// [x] It should have a way to display todos
-// [x] It should have a way to add new todos
-// [x] It should have a way to change a todo
-// [x] It should have a way to delete a todo
-
 let todos = ['item 1', 'item 2', 'item 3']
 
-console.log('My todos: ', todos)
+// [x] It should have a way to display todos
+// console.log('My todos: ', todos)
 
-todos.push('item 4', 'item 5')
+// [x] It should have a way to add new todos
+// todos.push('item 4', 'item 5')
 
-todos[0] = 'item 1 updated'
+// [x] It should have a way to change a todo
+// todos[0] = 'item 1 updated'
 
-todos.splice(0, 1)
+// [x] It should have a way to delete a todo
+// todos.splice(0, 1)
+
+// V2 Requirements
+// -------------------------------------
+// [x] It should have a function to display todos
+// Refactor this function definiton with an ES6 arrow function
+/*
+function displayTodos () {
+  console.log('My todos', todos)
+}
+*/
+const displayTodos = () => console.log('My todos:', todos)
+// displayTodos()
+
+// [x] It should have a function to add todos
+const addTodo = todo => {
+  todos.push(todo)
+  // See what's happened after adding a new todo
+  displayTodos()
+}
+// addTodo()
+
+// [x] It should have a function to change todos
+// Arrow functions SHOULD NOT return assignments
+// const changeTodo = (arrayIndex, newValue) => todos[arrayIndex] = 'newValue'
+function changeTodo (arrayIndex, newValue) {
+  todos[arrayIndex] = newValue
+  displayTodos()
+}
+
+// [] It should have a function to delete todos
+const deleteTodos = arrayIndex => {
+  todos.splice(arrayIndex, 1)
+  displayTodos()
+}
